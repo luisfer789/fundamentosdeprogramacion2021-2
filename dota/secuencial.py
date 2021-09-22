072623ff85fe52e45b8dff1b871fc209f6c2c56d
luisfer789 <>
2021-09-22 19:49:25 +0000

    juegos

---
+++ dota/secuencial.py
@@ -0,0 +1,9 @@
+print("Hallar el area de un trapecio")
+#Datos de entrada
+BM=int(input("Ingrese Base mayor del tra."))
+bm=int(input("Ingrese Base menor del tra."))
+h-int(input("Ingrese la altura"))
+#Proceso
+area-((BM*bm)*h)/2
+#Datos de salida
+print("El area del trapecio es:",area,"m2")
