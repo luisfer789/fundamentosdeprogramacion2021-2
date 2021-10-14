7a44714d1f4ca3b53587c779f87b6dd488b57c91
luisfer789 <>
2021-10-14 01:46:29 +0000

    ejer2

---
+++ ejercisios/ejer2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+
+public class ejer2{
+
+static Scanner sc = new Scanner(System.in);
+
+static void algoritmoRegalos(){
+  //Definir Variables
+  double monto=0;
+  String regalo="";
+  //Datos de entrada
+  System.out.println("Ingrese el monto que dispone:");  
+  monto=sc.nextDouble();
+  //Proceso
+  if(monto>=1 && monto<=10){
+    regalo="\nTarjeta";    
+  } else if(monto>=11 && monto<=100){
+    regalo="\nChocolate\nTarjeta";
+  }else if(monto>=101 && monto<=250){
+    regalo="\nFlores\nChocolate\nTarjeta";
+  }else if(monto>250){
+    regalo="\nAnillo\nFlores\nChocolate\nTarjeta";
+  }else{
+    regalo="Ninguno";
+  }
+  //Datos de salida
+  System.out.println("Ud. puede elegir los siguientes: "+ regalo);
+}
+
+public static void main(String[] args) {
+algoritmoRegalos();
+}
+}
