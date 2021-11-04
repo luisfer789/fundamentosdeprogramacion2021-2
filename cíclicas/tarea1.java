4d9323716311b93b5c02347f3d4ce06e11e7733a
luisfer789 <>
2021-11-04 06:09:56 +0000

    Mensaje1

--- cíclicas/tarea1.java
+++ cíclicas/tarea1.java
@@ -3,20 +3,12 @@ import java.util.Scanner;
 public class unodeluis{
 
 static Scanner sc = new Scanner(System.in);
-
-
   public static void main(String[] args) {
-    
-    
     int n=365;
-
-    
     for(int i = 1;i <=n;i++){
 
       double resultado = (double)(Math.pow(3, i));
-          
-          
-          
+           
       System.out.println("El ahorro del dia  "+i+"  es:  "+resultado+"  soles");
     }
     System.out.println("El ahorro de un anho  es:  "+(double)(Math.pow(3, n))+"  soles");
